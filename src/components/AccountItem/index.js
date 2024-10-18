77a38e4e7d0742fdfedfaf04e4140f14cb6c0ec2
Lê Minh Dương <leminhduong@MacBook-Pro-cua-Le.local>
2024-10-18 15:31:45 +0700

    header logical - search

--- src/components/AccountItem/index.js
+++ src/components/AccountItem/index.js
@@ -3,24 +3,21 @@ import styles from './AccountItem.module.scss';
 import Image from '˜/components/Images';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
 
 const cx = classNames.bind(styles);
-function AccountItem() {
+function AccountItem({ data }) {
     return (
-        <div className={cx('wrapper')}>
-            <Image
-                className={cx('avatar')}
-                src="https://p16-sign-useast2a.tiktokcdn.com/tos-useast2a-avt-0068-giso/907e06ea831a589a489123e5de7ef637~c5_300x300.webp?lk3s=a5d48078&nonce=75105&refresh_token=bd5fd87ed4e6c8ead627616f43111a50&x-expires=1727010000&x-signature=ETF%2FKfUWvvY%2FSkrM3gPJlnth5dU%3D&shp=a5d48078&shcp=c1333099"
-                alt="avatar"
-            />
+        <Link to={`/@${data.nickname}`} className={cx('wrapper')}>
+            <Image className={cx('avatar')} src={data.avatar} alt={data.full_name} />
             <div className={cx('info')}>
                 <h4 className={cx('name')}>
-                    <span>Nguyen Van A</span>
-                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+                    <span>{data.full_name}</span>
+                    {data.tick && <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />}
                 </h4>
-                <span className={cx('username')}>nguyenvana</span>
+                <span className={cx('username')}>{data.nickname}</span>
             </div>
-        </div>
+        </Link>
     );
 }
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -12,12 +12,11 @@ import Search from '˜/pages/Search';
 const publicRoutes = [
     { path: '/', component: Home },
     { path: '/following', component: Following },
-    { path: '/profile', component: Profile },
+    { path: '/:nickname', component: Profile },
     { path: '/upload', component: Upload, layout: HeaderOnly },
     { path: '/search', component: Search, layout: null },
 ];
 
-//private routes
 const privateRoutes = [];
 
 export { publicRoutes, privateRoutes };
